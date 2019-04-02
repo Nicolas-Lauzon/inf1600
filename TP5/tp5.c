525d7b22b057cd343b64b631d09a38cf6e32f4d0
nilau2 <nicolas-2.lauzon@polymtl.ca>
2019-04-02 10:50:17 -0400

    exercice 1 du TP5 complet. manque les commentaires

---
+++ TP5/tp5.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+unsigned int Decryption_fct(unsigned int le)
+{
+	unsigned int be; ///placé par défaut dans %eax
+
+	/*
+	 * Remplacez le code suivant par de l'assembleur en ligne
+	 * en utilisant le moins d'instructions possible
+	 */
+	 //be = (le & 0xff000000) | (le&0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;
+	 
+	
+
+	asm volatile (
+		"rol $8, %1 \n\t"
+		"bswap %1 \n\t"
+		: "=r"(be)// sorties (met le resultat par defaut dans la sortie)
+		: "r"(le)// entrées
+	);
+
+	return be;
+}
+
+int main()
+{
+	unsigned int data = 0xeeaabbff;
+
+	printf("Représentation crypte en little-endian:   %08x\n"
+	       "Représentation decrypte en big-endian:    %08x\n",
+	       data,
+	       Decryption_fct(data));
+
+	return 0;
+}
